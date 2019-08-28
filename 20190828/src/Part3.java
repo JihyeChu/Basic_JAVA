3cee9e9554bcd89159c8d04a6b698ee34bd978c1
JungGeun Ahn <arki13@naver.com>
2019-08-28 21:58:04 +0900

    start

---
+++ 20190828/src/Part3.java
@@ -0,0 +1,15 @@
+
+public class Part3 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		System.out.println("i = " + i);
+		
+		i = 300;
+		System.out.println("i = " + i);
+		
+		i = 2;
+		System.out.println("i = " + i);
+	}
+
+}
